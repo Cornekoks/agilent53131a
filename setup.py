d640c13d1b1948cc0cd3c85250a763b5d24c44bd
= <=>
2023-01-20 05:43:45 +0100

    add setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+from agilent53131a import __version__
+
+setup(
+    name='agilent53131a',
+    version=__version__,
+
+    url='https://github.com/Cornekoks/agilent53131a.git',
+    author='Corne Koks',
+    author_email='cornekoks0@gmail.com',
+
+    py_modules=['agilent53131a'],
+)
